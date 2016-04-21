bcf3566edd73a474ce50e05fbebf3adf9cb384e0
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-04-21 00:50:14 -0700

    Added section 2

--- js/javascript.js
+++ js/javascript.js
@@ -1,33 +1,19 @@
 
-	
-  $("#intro").backstretch("pic/intro2.jpg");
+  $("#intro").backstretch("pic/intro.jpg");
   $("#contact").backstretch("pic/aboutme.jpg");
   $('#about').backstretch('pic/aboutm.jpg');
 
 
+  $(document).scroll(function() {
+  var top = $('.intro-section').outerHeight()-2;
+    if( $(this).scrollTop() > top) {
+         $('.navbar-default').css("background-color", "rgba(0, 0, 0, 0.5)");
+         $('#my-nav').css("padding-top", "0");
+         $('#my-nav').removeClass('scrolled');
+    } else {
+        $('.navbar-default').css("background-color", "transparent");
+	    $('#my-nav').addClass('scrolled'); 
+	    $('#my-nav').css("padding-top", "10px");
+    }
+})
 
-        
-   $(document).ready(function(){                    
-        $(window).scroll(function(){                          
-            if ($(this).scrollTop()===0) {
-                $('.navbar-default').css("background-color", "transparent");
-            } else {
-				$('.navbar-default').css("background-color", "white");
-				
-			}
-        });
-		
-   })
-   
-  
-
-	$(window).scroll(function(){
-		if ($(window).scrollTop()===0){
-			$('#my-nav').removeClass('scrolled');
-		}
-		else{
-			$('#my-nav').addClass('scrolled');    
-		}
-	});
-	
-	
